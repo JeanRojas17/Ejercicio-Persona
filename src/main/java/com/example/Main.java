f85b2ce4af13c4df2b02fbb066c620f5bb088486
JeanRojas17 <jeanpaulrojas17@gmail.com>
2026-02-27 21:00:58 -0500

    Actualización del Main

--- src/main/java/com/example/Main.java
+++ src/main/java/com/example/Main.java
@@ -2,6 +2,14 @@ package com.example;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        
+        Persona persona1 = new Persona("Pedro", "Pérez", "1053121010", 1998);
+        Persona persona2 = new Persona("Luis", "León", "1053223344", 2001);
+
+        System.out.println("Información de la persona 1:");
+        persona1.imprimirInformacion();
+
+        System.out.println("\nInformación de la persona 2:");
+        persona2.imprimirInformacion();
     }
 }
